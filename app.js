c439ff834e9c7d15e5b0f95e100c1e3519be6539
吴云强 <yunqiang.wu@hand-china.com>
2018-01-05 12:49:40 +0800

    if();

--- app.js
+++ app.js
@@ -12,67 +12,68 @@ const app = websockify(new Koa());
 
 
 function broadcast(data) {
-  app.ws.server.clients.forEach(function each(client) {
-    if (client.readyState === 1) {
-      if(!client.registerOps || !data.body.repository){
-        console.log('not register', client.registerOps, data.body.repository);
-        client.send(JSON.stringify(data));
-        return;
-      }
-      let remoteBranch = data.body.ref && data.body.ref.replace('refs/heads/','');
-      let localBranch = client.registerOps.branch;
-      let remoteRepositoryName = data.body.repository.name;
-      let localRepositoryName = client.registerOps.repository;
-      let headers = JSON.parse(JSON.stringify(data.request.header).toLowerCase());
-      let gitEvent = headers['x-github-event']||headers['x-gitlab-event'];
-      gitEvent = gitEvent.replace('hook','').trim();
+    app.ws.server.clients.forEach(function each(client) {
+        if (client.readyState === 1) {
+            let registerOps = client.registerOps;
+            if (!registerOps || !data.body.repository) {
+                console.log('not register', registerOps);
+                client.send(JSON.stringify(data));
+                return;
+            }
+            let remoteBranch = data.body.ref && data.body.ref.replace('refs/heads/', '');
+            let localBranch = registerOps.branch;
+            let remoteRepositoryName = data.body.repository.name;
+            let localRepositoryName = registerOps.repository;
+            let headers = JSON.parse(JSON.stringify(data.request.header).toLowerCase());
+            let gitEvent = headers['x-github-event'] || headers['x-gitlab-event'];
+            gitEvent = gitEvent.replace('hook', '').trim();
 
 
-      if(remoteBranch !== localBranch ||  remoteRepositoryName !== localRepositoryName){
-        // console.log('');
-        console.log(`remoteBranch: ${remoteBranch} \tlocalBranch: ${localBranch}\nremoteRepositoryName: ${remoteRepositoryName}\tlocalRepositoryName: ${localRepositoryName}`);
-        return;
-      }
-      console.log("Hook Event: " + gitEvent);
-      if(gitEvent === 'push'){
-        client.send(JSON.stringify(data));
-      }
-    }
-  });
+            if (remoteBranch !== localBranch || remoteRepositoryName !== localRepositoryName) {
+                // console.log('');
+                console.log(`remoteBranch: ${remoteBranch} \tlocalBranch: ${localBranch}\nremoteRepositoryName: ${remoteRepositoryName}\tlocalRepositoryName: ${localRepositoryName}`);
+                return;
+            }
+            console.log("Hook Event: " + gitEvent);
+            if (gitEvent === 'push') {
+                client.send(JSON.stringify(data));
+            }
+        }
+    });
 };
 
 app.use(bodyParser());
 
 
-app.use(async (ctx, next) => {
+app.use(async(ctx, next) => {
 
-  let message = {
-  	request: ctx.request,
-  	body: ctx.request.body,
-  };
-  // let messageStr = JSON.stringify(message,null,2);
+    let message = {
+        request: ctx.request,
+        body: ctx.request.body,
+    };
+    // let messageStr = JSON.stringify(message,null,2);
 
-  if(ctx.request.url.startsWith("/hook")){
-    setTimeout(()=>{
-      broadcast(message);
-    },0);
-  	ctx.body = "{}"
-  	return;
-  }
+    if (ctx.request.url.startsWith("/hook")) {
+        setTimeout(() => {
+            broadcast(message);
+        }, 0);
+        ctx.body = "{}"
+        return;
+    }
 
-  if(ctx.request.url.startsWith("/health")){
-    ctx.body = "{}"
-    return;
-  }
-  // console.log(messageStr);
-  await next();
+    if (ctx.request.url.startsWith("/health")) {
+        ctx.body = "{}"
+        return;
+    }
+    // console.log(messageStr);
+    await next();
 });
 
-app.use(serve(path.join(__dirname,'public')));
+app.use(serve(path.join(__dirname, 'public')));
 
 const redirect = ctx => {
-  ctx.response.redirect('/index.html');
-  ctx.response.body = '<a href="/index.html">Index Page</a>';
+    ctx.response.redirect('/index.html');
+    ctx.response.body = '<a href="/index.html">Index Page</a>';
 };
 
 app.use(route.get('/', redirect));
@@ -83,36 +84,37 @@ app.listen(PORT);
 
 
 app.ws.server.on('connection', function connection(ws) {
-  console.log('client 连接成功:');
-  ws.on('message', function incoming(data) {
-    // Broadcast to everyone else.
-    data = JSON.parse(data);
-    if(data.action === 'reg');
-    {
-      ws.registerOps = data.data;
-    }
-  });
+    console.log('client 连接成功:');
+    ws.on('message', function incoming(data) {
+        // Broadcast to everyone else.
+        data = JSON.parse(data);
+        if (data.action === 'reg') {
+            ws.registerOps = data.data;
+            console.log('client 注册成功:', ws.registerOps);
+        }
+        // console.log(ws,data);
+    });
 
-  ws.on('close', function incoming(data) {
-    // Broadcast to everyone else.
-    console.log('client 连接断开:');
-  });
+    ws.on('close', function incoming(data) {
+        // Broadcast to everyone else.
+        console.log('client 连接断开:', ws.registerOps);
+    });
 
 });
 
 
-  // "devDependencies": {
-  //   "babel-plugin-syntax-async-functions": "^6.13.0",
-  //   "babel-plugin-transform-async-to-generator": "^6.24.1",
-  //   "babel-preset-es2015": "^6.24.1",
-  //   "babel-preset-stage-2": "^6.24.1",
-  //   "babel-register": "^6.26.0"
-  // }
+// "devDependencies": {
+//   "babel-plugin-syntax-async-functions": "^6.13.0",
+//   "babel-plugin-transform-async-to-generator": "^6.24.1",
+//   "babel-preset-es2015": "^6.24.1",
+//   "babel-preset-stage-2": "^6.24.1",
+//   "babel-register": "^6.26.0"
+// }
 
- //  {
- //    "presets": [
- //      "es2015",
- //      "stage-3"
- //    ],
- //    "plugins": ["syntax-async-generators","transform-async-to-generator","syntax-async-functions","transform-koa2-async-to-generator"]
- // }
+//  {
+//    "presets": [
+//      "es2015",
+//      "stage-3"
+//    ],
+//    "plugins": ["syntax-async-generators","transform-async-to-generator","syntax-async-functions","transform-koa2-async-to-generator"]
+// }
